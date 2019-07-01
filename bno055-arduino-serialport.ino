1bcd66ba4fa1b441ebeff3f636de3b9602b52c36
Francisco Bernardo <f.bernardo@sussex.ac.uk>
2019-07-01 06:13:52 +0100

    changes to ports; added optional print label

--- bno055-arduino-serialport.ino
+++ bno055-arduino-serialport.ino
@@ -18,13 +18,10 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 int AnalogPin0 = A0; //Declare an integer variable, hooked up to analog pin 0
 int AnalogPin1 = A1; //Declare an integer variable, hooked up to analog pin 1
-int AnalogPin2 = A2; //Declare an integer variable, hooked up to analog pin 2
-
 
 void setup(void) 
 {
-  Serial.begin(9600); //Begin Serial Communication with a baud rate of 9600
-  
+  Serial.begin(9600); //Begin Serial Communication with a baud rate of 9600  
   setupBNO055();
 }
 
@@ -33,16 +30,16 @@ void loop(void)
 {
   int AnalogPin0Value = analogRead(AnalogPin0);
   int AnalogPin1Value = analogRead(AnalogPin1);
-  int AnalogPin2Value = analogRead(AnalogPin2);
-
-  Serial.print(AnalogPin0Value, DEC); 
-  Serial.print(",");
-  Serial.print(AnalogPin1Value, DEC); 
-  Serial.print(",");
-  Serial.print(AnalogPin2Value, DEC); 
-  Serial.print(",");
+//  int AnalogPin2Value = analogRead(AnalogPin2);
+//
+//  Serial.print(AnalogPin0Value, DEC); 
+//  Serial.print(",");
+//  Serial.print(AnalogPin1Value, DEC); 
+//  Serial.print(",");
+//  Serial.print(AnalogPin2Value, DEC); 
+//  Serial.print(",");
   
-  send_IMU_Quaternion();
+  send_IMU_Quaternion(false);
 //  Serial.print(",");
 //  send_Euler_degrees();  
 //  Serial.print(",");
@@ -68,7 +65,7 @@ void setupBNO055()
   if(!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
-//    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
   
@@ -100,79 +97,79 @@ void send_Euler_degrees()
   Serial.print(euler.z(), DEC);
 }
 
-void send_IMU_Quaternion()
+void send_IMU_Quaternion(bool printLabels)
 {
     /* Display the quat data */
   imu::Quaternion quat = bno.getQuat();
   
-//  Serial.print("qW: ");
+  if(printLabels) Serial.print("qW: ");
   Serial.print(quat.w(), DEC);
   Serial.print(",");
-//  Serial.print(" qX: ");
+  if(printLabels) Serial.print(" qX: ");
   Serial.print(quat.y(), DEC);
   Serial.print(",");
-//  Serial.print(" qY: ");
+  if(printLabels) Serial.print(" qY: ");
   Serial.print(quat.x(), DEC);
   Serial.print(",");
-//  Serial.print(" qZ: ");
+  if(printLabels) Serial.print(" qZ: ");
   Serial.print(quat.z(), DEC);
 //  Serial.print(",");
 //  Serial.println("");
 }
 
-void send_Accelerometer()
+void send_Accelerometer(bool printLabels)
 {
     imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
   
   /* Display the floating point data - m/s^2 */
-//  Serial.print("X: ");
+  if(printLabels) Serial.print("X: ");
   Serial.print(accel.x(), DEC);
   Serial.print(",");
-//  Serial.print(" Y: ");
+  if(printLabels) Serial.print(" Y: ");
   Serial.print(accel.y(), DEC);
   Serial.print(",");
-//  Serial.print(" Z: ");
+  if(printLabels) Serial.print(" Z: ");
   Serial.print(accel.z(), DEC);
 }
 
-void send_Magnetometer()
+void send_Magnetometer(bool printLabels)
 {
   /* Display the Euler degrees */
   imu::Vector<3> magnetometer = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
   /* Display the floating point data - uT */
-//  Serial.print("X: ");
+  if(printLabels) Serial.print("magnetometer_X: ");
   Serial.print(magnetometer.x(), DEC);
   Serial.print(",");
-//  Serial.print(" Y: ");
+  if(printLabels) Serial.print("magnetometer_Y: ");
   Serial.print(magnetometer.y(), DEC);
   Serial.print(",");
-//  Serial.print(" Z: ");
+  if(printLabels) Serial.print("magnetometer_Z: ");
   Serial.print(magnetometer.z(), DEC);
 }
 
-void send_Current_Temperature()
+void send_Current_Temperature(bool printLabels)
 {
   /* Display the current temperature - C */
   int8_t temp = bno.getTemp();
   
-//  Serial.print("Current Temperature: ");
+  if(printLabels) Serial.print("Current Temperature: ");
   Serial.print(temp);
 //  Serial.print(" C");
 }
 
-void send_Gravity()
+void send_Gravity(bool printLabels)
 {
   /* Display the gravity -  m/s^2 */
   imu::Vector<3> gravity = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
   
   /* Display the floating point data */
-//  Serial.print("X: ");
+  if(printLabels) Serial.print("Gravity_X (m/s^2): ");
   Serial.print(gravity.x(), DEC);
   Serial.print(",");
-//  Serial.print(" Y: ");
+  if(printLabels) Serial.print("Gravity_Y: ");
   Serial.print(gravity.y(), DEC);
   Serial.print(",");
-//  Serial.print(" Z: ");
+  if(printLabels) Serial.print("Gravity_Y: ");
   Serial.print(gravity.z(), DEC);
 }
